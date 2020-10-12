b23078ab4c702ac27f35a6e8ca0f9c73bd3c0286
abdoul <abdoulazizzorom@gmail.com>
2020-10-12 13:05:55 +0000

    First Commit

--- src/views/admin/default.blade.php
+++ src/views/admin/default.blade.php
@@ -1,26 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <!-- Basic Page Info -->
     <meta charset="utf-8">
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Mobile Specific Metas -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-    <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
-    <!-- CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
     <link rel="stylesheet" type="text/css" href="{{ asset('vendor/posts-manager/css/style.css') }}">
-
-    {{-- <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
-    <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" /> --}}
 
 
 </head>
@@ -69,8 +61,6 @@
             </p>
         </div>
     </footer>
-
-
 
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 
